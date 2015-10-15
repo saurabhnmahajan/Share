609327be17151e8ed1825737074dd67c29f0bcd0
saurabh mahajan <saurabh.nmahajan@gmail.com>
2015-10-15 19:04:51 +0530

    custom marker for my location added

--- app/src/main/java/com/example/home/share/MapsActivity.java
+++ app/src/main/java/com/example/home/share/MapsActivity.java
@@ -1,17 +1,24 @@
 package com.example.home.share;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.maps.android.ui.IconGenerator;
 
 public class MapsActivity extends FragmentActivity {
 
@@ -24,7 +31,6 @@ public class MapsActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         setUpMapIfNeeded();
-        db.getAllContacts();
     }
 
     @Override
@@ -48,10 +54,20 @@ public class MapsActivity extends FragmentActivity {
 
     private void setUpMap() {
         mMap.setMyLocationEnabled(true);
-//        IconGenerator i = new IconGenerator(getApplicationContext());
-//        i.setStyle(IconGenerator.STYLE_BLUE);
-//        i.setRotation(-90);
-//        final Bitmap bitmap = i.makeIcon("hi");
+        IconGenerator i = new IconGenerator(getApplicationContext());
+        i.setStyle(IconGenerator.STYLE_BLUE);
+        // Define the size you want from dimensions file
+        int shapeSize = getResources().getDimensionPixelSize(R.dimen.shape_size);
+
+        Drawable shapeDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.circle_marker, null);
+        i.setBackground(shapeDrawable);
+
+// Create a view container to set the size
+        View view = new View(this);
+        view.setLayoutParams(new ViewGroup.LayoutParams(shapeSize, shapeSize));
+        i.setContentView(view);
+        i.setTextAppearance(IconGenerator.STYLE_GREEN);
+        final Bitmap bitmap = i.makeIcon();
         mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
             @Override
             public void onMyLocationChange(Location location) {
@@ -65,8 +81,9 @@ public class MapsActivity extends FragmentActivity {
                 final LatLng latLng = new LatLng(latitude, longitude);
                 if (flag) {
                     mMap.addMarker(new MarkerOptions().position(latLng)
-//                            .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
-                    );
+                            .anchor(0.5f, 0.5f)
+//                            .icon(BitmapDescriptorFactory.fromBitmap(R)));
+                            .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
 
                     mMap.getUiSettings().setZoomControlsEnabled(true);
                     com.google.android.gms.maps.model.LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
