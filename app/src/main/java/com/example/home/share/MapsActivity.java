cfe00792f3e9d127e60e21122dfe48af687a74e5
saurabh mahajan <saurabh.nmahajan@gmail.com>
2015-10-16 00:26:06 +0530

    updated my location code

--- app/src/main/java/com/example/home/share/MapsActivity.java
+++ app/src/main/java/com/example/home/share/MapsActivity.java
@@ -2,14 +2,15 @@ package com.example.home.share;
 
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,12 +21,14 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.ui.IconGenerator;
 
-public class MapsActivity extends FragmentActivity {
+public class MapsActivity extends FragmentActivity implements LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private boolean flag = true;
     CameraUpdate zoomLvl;
     DatabaseHandler db = new DatabaseHandler(this);
+    Location myLocation;
+    LocationManager locationManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,51 +56,72 @@ public class MapsActivity extends FragmentActivity {
     }
 
     private void setUpMap() {
-        mMap.setMyLocationEnabled(true);
+        mMap.setMyLocationEnabled(false);
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+
+        Location location = locationManager.getLastKnownLocation(bestProvider);
+        locationManager.requestLocationUpdates(bestProvider, 0, 0, this);
+        if (location != null) {
+            onLocationChanged(location);
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
         IconGenerator i = new IconGenerator(getApplicationContext());
-        i.setStyle(IconGenerator.STYLE_BLUE);
         // Define the size you want from dimensions file
         int shapeSize = getResources().getDimensionPixelSize(R.dimen.shape_size);
 
         Drawable shapeDrawable = ResourcesCompat.getDrawable(getResources(), R.drawable.circle_marker, null);
         i.setBackground(shapeDrawable);
 
-// Create a view container to set the size
+        // Create a view container to set the size
         View view = new View(this);
         view.setLayoutParams(new ViewGroup.LayoutParams(shapeSize, shapeSize));
-        i.setContentView(view);
-        i.setTextAppearance(IconGenerator.STYLE_GREEN);
-        final Bitmap bitmap = i.makeIcon();
-        mMap.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-            @Override
-            public void onMyLocationChange(Location location) {
-                double latitude = location.getLatitude();
-                // Getting longitude of the current location
-                double longitude = location.getLongitude();
-                // Creating a LatLng object for the current location
-                Bundle b = getIntent().getExtras();
-                String user = b.getString("user");
-                db.updateUserLocation(user, latitude, longitude);
-                final LatLng latLng = new LatLng(latitude, longitude);
-                if (flag) {
-                    mMap.addMarker(new MarkerOptions().position(latLng)
-                            .anchor(0.5f, 0.5f)
-//                            .icon(BitmapDescriptorFactory.fromBitmap(R)));
-                            .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
 
-                    mMap.getUiSettings().setZoomControlsEnabled(true);
-                    com.google.android.gms.maps.model.LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
-                    boundsBuilder.include(latLng);
-                    LatLngBounds bounds = boundsBuilder.build();
-                    zoomLvl = CameraUpdateFactory.newLatLngBounds(bounds, 100);
-                    mMap.animateCamera(zoomLvl);
-                    float zoom = mMap.getCameraPosition().zoom;
-                    Toast.makeText(getApplicationContext(), zoom + "", Toast.LENGTH_SHORT).show();
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16.0f));
-                    Toast.makeText(getApplicationContext(), zoom + "", Toast.LENGTH_SHORT).show();
-                    flag = false;
-                }
-            }
-        });
+        i.setContentView(view);
+        final Bitmap bitmap = i.makeIcon();
+
+        double latitude = location.getLatitude();
+        // Getting longitude of the current location
+        double longitude = location.getLongitude();
+        // Creating a LatLng object for the current location
+        Bundle b = getIntent().getExtras();
+        String user = b.getString("user");
+        db.updateUserLocation(user, latitude, longitude);
+        final LatLng latLng = new LatLng(latitude, longitude);
+        if (flag) {
+            flag = false;
+            mMap.addMarker(new MarkerOptions().position(latLng)
+                    .anchor(0.5f, 0.5f)
+                    .icon(BitmapDescriptorFactory.fromBitmap(bitmap)));
+            mMap.getUiSettings()
+                    .setMyLocationButtonEnabled(false);
+            mMap.getUiSettings()
+                    .setAllGesturesEnabled(true);
+            com.google.android.gms.maps.model.LatLngBounds.Builder boundsBuilder = new LatLngBounds.Builder();
+            boundsBuilder.include(latLng);
+            LatLngBounds bounds = boundsBuilder.build();
+            zoomLvl = CameraUpdateFactory.newLatLngBounds(bounds, 10 , 10, 0);
+            mMap.animateCamera(zoomLvl);
+            float zoom = mMap.getCameraPosition().zoom;
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 }
