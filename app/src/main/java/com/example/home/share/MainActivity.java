e397b1115b42087abfe5d28ec40d56da1d4a4814
saurabh mahajan <saurabh.nmahajan@gmail.com>
2015-10-19 16:52:01 +0530

    facebook login integrated

--- app/src/main/java/com/example/home/share/MainActivity.java
+++ app/src/main/java/com/example/home/share/MainActivity.java
@@ -3,11 +3,40 @@ package com.example.home.share;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 public class MainActivity extends Activity {
+    CallbackManager mCallBackManager;
+    FacebookCallback<LoginResult> mCallback = new FacebookCallback<LoginResult>() {
+        @Override
+        public void onSuccess(LoginResult loginResult) {
+            AccessToken accessToken = loginResult.getAccessToken();
+            Profile profile = Profile.getCurrentProfile();
+            Log.d("fb login", profile.getId());
+        }
+
+        @Override
+        public void onCancel() {
+
+        }
+
+        @Override
+        public void onError(FacebookException e) {
+
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +57,18 @@ public class MainActivity extends Activity {
                 startActivity(intent);
             }
         });
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        mCallBackManager = CallbackManager.Factory.create();
+        LoginButton fb_login = (LoginButton)findViewById(R.id.fb_login_button);
+        fb_login.setReadPermissions("user_friends");
+        fb_login.registerCallback(mCallBackManager, mCallback);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        mCallBackManager.onActivityResult(requestCode, resultCode, data);
     }
 }
